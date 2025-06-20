5ba51af55e8823bde65e456978c2c4c4f39e6b1c
RUB3N02 <rubyerizo@gmail.com>
2025-06-20 14:16:42 +0000

    º2 parte intente de mejora

--- src/app.js
+++ src/app.js
@@ -51,5 +51,42 @@ const checkhandGuess = (userGuess) => {
 const guess = prompt("Guess: rock, paper, scissors, lizard or spock.").toLowerCase();
 checkhandGuess(guess);
 
-
+//const hand = ["rock", "paper", "scissors", "lizard", "spock"];
+//
+//const getRandomHand = () => {
+//    const index = Math.floor(Math.random() * hand.length);
+//    return hand[index];
+//};
+//
+//const checkHandGuess = (userGuess) => {
+//    userGuess = userGuess.toLowerCase();
+//
+//    if (!hand.includes(userGuess)) {
+//        console.log("Only rock, paper, scissors, lizard or spock are valid options.");
+//        return;
+//    }
+//
+//    const actualHand = getRandomHand();
+//    console.log(`You chose: ${userGuess}`);
+//    console.log(`Opponent chose: ${actualHand}`);
+//
+//    if (userGuess === actualHand) {
+//        console.log("Draw");
+//    } else if (rules[userGuess].includes(actualHand)) {
+//        console.log("You win!");
+//    } else {
+//        console.log("You lose.");
+//    }
+//};
+//
+//const rules = {
+//    rock: ["scissors", "lizard"],
+//    paper: ["rock", "spock"],
+//    scissors: ["paper", "lizard"],
+//    lizard: ["spock", "paper"],
+//    spock: ["scissors", "rock"]
+//};
+//
+//const guess = prompt("Guess: rock, paper, scissors, lizard or spock.").toLowerCase();
+//checkHandGuess(guess);
 
